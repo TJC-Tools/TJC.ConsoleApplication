463877fb026834af03fe05dbead1f847e91d0795
Tyler Carrol <tylercarrol@hotmail.com>
2024-07-24 20:32:25 -0400

    Sample Program & README

--- SampleConsoleApplication/GlobalUsings.cs
+++ SampleConsoleApplication/GlobalUsings.cs
@@ -1,7 +1,9 @@
 ﻿global using SampleConsoleApplication;
 global using SampleConsoleApplication.Enums;
 global using System.Reflection;
+global using TJC.ConsoleApplication;
 global using TJC.ConsoleApplication.Arguments.Extensions;
 global using TJC.ConsoleApplication.Arguments.Options;
+global using TJC.ConsoleApplication.Arguments.Options.Specific;
 global using TJC.ConsoleApplication.Exit;
 global using TJC.ConsoleApplication.Inputs;
