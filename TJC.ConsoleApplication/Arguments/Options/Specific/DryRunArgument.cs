3a2e88c1cb0305299e53d85a1aa0c8d939a13bab
Tyler Carrol <tylercarrol@hotmail.com>
2024-07-24 20:29:56 -0400

    Add specific arguments

---
+++ TJC.ConsoleApplication/Arguments/Options/Specific/DryRunArgument.cs
@@ -0,0 +1,19 @@
+﻿namespace TJC.ConsoleApplication.Arguments.Options.Specific;
+
+/// <summary>
+/// This argument is intended for applications that require the option to run a test process that doesn't make any changes.
+/// <para>But, writes logs based on what it would have done.</para>
+/// </summary>
+public class DryRunArgument : IConsoleArgument
+{
+    private const string Prototype = "dry-run|dryrun";
+
+    public static DryRunArgument Default => new();
+
+    public DryRunArgument(string description = "Run through the process, writing logs, but do not execute any changes") =>
+        Argument = new ConsoleArgument(null, Prototype, v => DryRun = !string.IsNullOrEmpty(v), isRequired: false, description);
+
+    public bool DryRun { get; private set; }
+
+    public ConsoleArgument Argument { get; }
+}
