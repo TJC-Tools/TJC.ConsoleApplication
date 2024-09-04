ee32d20b62166a81bd55c0b8aa5c694169de78a2
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-03 21:57:58 -0400

    Add License Argument

--- SampleConsoleApplication/Arguments.cs
+++ SampleConsoleApplication/Arguments.cs
@@ -12,12 +12,13 @@ internal class Arguments
     internal static DryRunArgument DryRun => DryRunArgument.Default; // Predefined argument that is used in multiple applications
     internal static VersionArgument Version => VersionArgument.Default; // Predefined argument that is used in multiple applications
     internal static CopyrightArgument Copyright => CopyrightArgument.Default; // Predefined argument that is used in multiple applications
+    internal static LicenseArgument License => LicenseArgument.Default; // Predefined argument that is used in multiple applications
     internal static bool Item1 { get; private set; }
     internal static string? Item2 { get; private set; }
 
     internal static readonly ConsoleArguments ConsoleArguments = new(flagRequired: true, logParsedOptions: false)
     {
-        DryRun, Version, Copyright,
+        DryRun, Version, Copyright, License,
         { "item1", v => Item1 = !string.IsNullOrEmpty(v), "Example Item 1" },
         { "item2", v => Item2 = v, "Example Item 2" }
     };
---
+++ TJC.ConsoleApplication/Arguments/Options/Specific/LicenseArgument.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using TJC.AssemblyExtensions.Attributes;
+
+namespace TJC.ConsoleApplication.Arguments.Options.Specific;
+
+/// <summary>
+/// This argument is intended to print the license of the application.
+/// </summary>
+public class LicenseArgument : IConsoleArgument
+{
+    private const string Prototype = "license";
+
+    public static LicenseArgument Default => new();
+
+    public LicenseArgument(string description = "Print the license of the application") =>
+        Argument = new ConsoleArgument(null, Prototype, v =>
+        {
+            Selected = true;
+            Execute();
+        }, isRequired: false, description);
+
+    public bool Selected { get; private set; }
+
+    public ConsoleArgument Argument { get; }
+
+    public void Execute()
+    {
+        if (!Selected)
+            return;
+        var assembly = Assembly.GetEntryAssembly();
+        var license = assembly?.GetLicense();
+        ConsoleOutputHandler.WriteLine($"{license}");
+    }
+}
