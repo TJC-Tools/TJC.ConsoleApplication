1f4cd447efe8497e2bb7bdd90f0b43c0f4d93839
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-08 19:05:28 -0400

    ✨ feature: Version Digit Display Setting (Default: 3) (#51)
    * ✨ feature: Add Setting for Version Digits
    
    * ✨ feature: Update Version Display to use Version Digits
    
    * 🧹 chore: Formatting Projects
    
    * 🚨 tests: Fix test by setting version display digits to 4

--- TJC.ConsoleApplication/Settings/ConsoleSettings.cs
+++ TJC.ConsoleApplication/Settings/ConsoleSettings.cs
@@ -29,5 +29,13 @@ public class ConsoleSettings
     /// </summary>
     public bool TraceToConsole { get; set; } = true;
 
+    /// <summary>
+    /// The number of digits to display for the version.
+    /// Formats:
+    /// 3: v{major}.{minor}.{patch}.
+    /// 4: v{major}.{minor}.{build}.{revision}
+    /// </summary>
+    public int VersionDigits { get; set; } = 3;
+
     #endregion
 }
