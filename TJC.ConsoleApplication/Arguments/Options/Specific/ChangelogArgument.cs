9c10b86033db5d2c4a11474cf22cc9ad972e6323
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-10 22:41:53 -0400

    ✨ feature: Add Changelog Options

--- TJC.ConsoleApplication/Arguments/Options/Specific/ChangelogArgument.cs
+++ TJC.ConsoleApplication/Arguments/Options/Specific/ChangelogArgument.cs
@@ -10,9 +10,18 @@ public class ChangelogArgument : IConsoleArgument
 {
     private const string Prototype = "changelog";
 
+    private readonly bool _includeHeader;
+    private readonly bool _includeUnreleasedSection;
+    private readonly bool _includePaths;
+
     public static ChangelogArgument Default => new();
 
-    public ChangelogArgument(string description = "Print the changelog of the application", bool exitIfUsed = true) =>
+    public ChangelogArgument(string description = "Print the changelog of the application",
+                             bool exitIfUsed = true,
+                             bool includeHeader = false,
+                             bool includeUnreleasedSection = false,
+                             bool includePaths = false)
+    {
         Argument = new ConsoleArgument(null, Prototype, v =>
         {
             Selected = true;
@@ -21,6 +30,11 @@ public class ChangelogArgument : IConsoleArgument
         isRequired: false,
         description: description,
         exitIfUsed: exitIfUsed);
+
+        _includeHeader = includeHeader;
+        _includeUnreleasedSection = includeUnreleasedSection;
+        _includePaths = includePaths;
+    }
 
     public bool Selected { get; private set; }
 
@@ -31,7 +45,10 @@ public class ChangelogArgument : IConsoleArgument
         if (!Selected)
             return;
         var assembly = Assembly.GetEntryAssembly();
-        var changelog = assembly?.GetChangelog();
+        var changelog = assembly?.GetChangelog(
+            includeHeader: _includeHeader,
+            includeUnreleasedSection: _includeUnreleasedSection,
+            includePaths: _includePaths);
         ConsoleOutputHandler.WriteLine($"{changelog}");
     }
 }
