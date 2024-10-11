5778faed150bc2eefb9158927eccc5c7eec2b282
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-10 20:40:19 -0400

    ✨ feature: Add Argument for Changelog

---
+++ TJC.ConsoleApplication/Arguments/Options/Specific/ChangelogArgument.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using TJC.AssemblyExtensions.Attributes;
+
+namespace TJC.ConsoleApplication.Arguments.Options.Specific;
+
+/// <summary>
+/// This argument is intended to print the changelog of the application.
+/// </summary>
+public class ChangelogArgument : IConsoleArgument
+{
+    private const string Prototype = "changelog";
+
+    public static ChangelogArgument Default => new();
+
+    public ChangelogArgument(string description = "Print the changelog of the application") =>
+        Argument = new ConsoleArgument(null, Prototype, v =>
+        {
+            Selected = true;
+            Execute();
+        }, isRequired: false, description);
+
+    public bool Selected { get; private set; }
+
+    public ConsoleArgument Argument { get; }
+
+    public void Execute()
+    {
+        if (!Selected)
+            return;
+        var assembly = Assembly.GetEntryAssembly();
+        var changelog = assembly?.GetChangelog();
+        ConsoleOutputHandler.WriteLine($"{changelog}");
+    }
+}
