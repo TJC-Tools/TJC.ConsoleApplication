d129ee4a2dbb7ab1ebfb7eb9fdc2908bc55fe759
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-23 21:08:05 -0400

    🚨 tests: Add Test for Help Argument

--- TJC.ConsoleApplication.Tests/Options/ConsoleArgumentsTests.cs
+++ TJC.ConsoleApplication.Tests/Options/ConsoleArgumentsTests.cs
@@ -1,4 +1,4 @@
-using TJC.ConsoleApplication.Arguments.Options;
+﻿using TJC.ConsoleApplication.Arguments.Options;
 using TJC.ConsoleApplication.Arguments.Options.Specific;
 
 namespace TJC.ConsoleApplication.Tests.Options
@@ -69,6 +69,17 @@ namespace TJC.ConsoleApplication.Tests.Options
         {
             // Arrange
             var argument = DryRunArgument.Default;
+            _ = new ConsoleArguments { argument };
+
+            // Assert
+            Assert.IsTrue(argument.Argument.HasParent);
+        }
+
+        [TestMethod]
+        public void ConstructConsoleArguments_Help_HasParent()
+        {
+            // Arrange
+            var argument = HelpArgument.Default;
             _ = new ConsoleArguments { argument };
 
             // Assert
