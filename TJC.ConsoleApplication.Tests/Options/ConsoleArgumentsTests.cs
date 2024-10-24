dbf88ed67d4351a737e6e11492a64336b7a134b8
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-23 20:32:00 -0400

    🚨 tests: Create Test for Console Arguments Has Parents

---
+++ TJC.ConsoleApplication.Tests/Options/ConsoleArgumentsTests.cs
@@ -0,0 +1,45 @@
+﻿using TJC.ConsoleApplication.Arguments.Options;
+using TJC.ConsoleApplication.Arguments.Options.Specific;
+
+namespace TJC.ConsoleApplication.Tests.Options
+{
+    [TestClass]
+    public class ConsoleArgumentsTests
+    {
+        [TestMethod]
+        public void ConstructConsoleArguments_ArgumentCountIs3()
+        {
+            // Arrange
+            var arguments = new ConsoleArguments()
+            {
+                DryRunArgument.Default,
+                ChangelogArgument.Default,
+                LicenseArgument.Default
+            };
+
+            // Act
+            var result = arguments.Count;
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void ConstructConsoleArguments_AllHaveParent()
+        {
+            // Arrange
+            var arguments = new ConsoleArguments()
+            {
+                DryRunArgument.Default,
+                ChangelogArgument.Default,
+                LicenseArgument.Default
+            };
+
+            // Act
+            var result = arguments.All(x => x.HasParent);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
