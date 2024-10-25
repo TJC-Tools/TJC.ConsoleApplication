e850dd59878304ff8c717ed1f7ea8796a63e58bb
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-24 22:13:52 -0400

    🚨 tests: Add Enum Argument Tests

---
+++ TJC.ConsoleApplication.Tests/Options/EnumArgumentTests.cs
@@ -0,0 +1,37 @@
+﻿using TJC.ConsoleApplication.Arguments.Extensions;
+using TJC.ConsoleApplication.Arguments.Options;
+using TJC.ConsoleApplication.Arguments.Options.Specific;
+
+namespace TJC.ConsoleApplication.Tests.Options;
+
+[TestClass]
+public class EnumArgumentTests
+{
+    [TestMethod]
+    public void ParseEnumArgument_ReturnsOption2()
+    {
+        // Arrange
+        var argument = new EnumArgument<SampleEnumChoices>();
+        var arguments = new ConsoleArguments { argument };
+
+        // Act
+        arguments.ParseAndValidate(["--option2"], exitOnFailureToParse: false);
+
+        // Assert
+        Assert.AreEqual(SampleEnumChoices.Option2, argument.SelectedEnumValue);
+    }
+
+    [TestMethod]
+    public void ParseEnumArgument_ReturnsOption3()
+    {
+        // Arrange
+        var argument = new EnumArgument<SampleEnumChoices>();
+        var arguments = new ConsoleArguments { argument };
+
+        // Act
+        arguments.ParseAndValidate(["--option3"], exitOnFailureToParse: false);
+
+        // Assert
+        Assert.AreEqual(SampleEnumChoices.Option3, argument.SelectedEnumValue);
+    }
+}
