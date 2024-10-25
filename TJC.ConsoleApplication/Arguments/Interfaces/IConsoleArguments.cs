7b41f1304fa2003eb7a071e53dcf6322523e0d59
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-25 17:18:38 -0400

    Create new Interface

---
+++ TJC.ConsoleApplication/Arguments/Interfaces/IConsoleArguments.cs
@@ -0,0 +1,12 @@
+﻿namespace TJC.ConsoleApplication.Arguments.Interfaces;
+
+public interface IConsoleArguments : IEnumerable<ConsoleArgument>
+{
+    /// <summary>
+    /// Parses Options, and Validates that there are no Invalid or Missing Arguments
+    /// </summary>
+    /// <param name="args">Arguments from console application call</param>
+    /// <param name="programName">Name of Program</param>
+    /// <param name="exitOnFailureToParse">Exit Program on Failure to Parse</param>
+    void ParseAndValidate(string[] args, string? programName = null, bool exitOnFailureToParse = true);
+}
