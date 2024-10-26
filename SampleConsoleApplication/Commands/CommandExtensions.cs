e7d42a1d9949d2a548845852acf95d6faf0a74d1
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-25 22:39:29 -0400

    ✨ feature: Create Command Extensions for Help Menu

---
+++ SampleConsoleApplication/Commands/CommandExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleConsoleApplication.Commands;
+
+internal static class CommandExtensions
+{
+    internal static string GetCommandHelp(this CommandTypes commandType)
+    {
+        return commandType switch
+        {
+            CommandTypes.Test => "This is the help for Test.",
+            CommandTypes.Run => "This is the help for Run.",
+            CommandTypes.Validate => "This is the help for Validate.",
+            CommandTypes.GenerateValue => "This is the help for Generate Value.",
+            CommandTypes.ListItems => "This is the help for List Items.",
+            _ => string.Empty
+        };
+    }
+}
